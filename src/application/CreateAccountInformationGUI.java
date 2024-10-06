80d649b0a35230062e6f1f1be927d1afbedb798e
Eyan <eyanmartucci11@gmail.com>
2024-10-05 20:50:49 -0700

    Added Classes
    - Added UpdateAccountInformationGUI class and pasted code from previous repository
    - Added empty AccountData class

---
+++ src/application/CreateAccountInformationGUI.java
@@ -0,0 +1,43 @@
+package application;
+
+public class CreateAccountInformationGUI {
+
+	/**
+	 * Variable setup
+	 */
+	//test
+	/*
+	private String emailInput;
+	private String firstNameInput;
+	private String middleNameInput;
+	private String lastNameInput;
+	private String prefNameInput;
+	
+	private Label emailLabel;
+	private Label firstNameLabel;
+	private Label middleNameLabel;
+	private Label lastNameLabel;
+	private Label prefNameLabel;
+	
+	private Text emailTest;
+	private Text firstNameText;
+	private Text middleNameText;
+	private Text lastNameText;
+	private Text prefnameText;
+	
+	private Button updateButton;
+	*/
+	
+	/** Default constructor
+	 */
+	public void userInfo() {
+		
+	}
+	
+	public void userInfo(String email, String firstName, String middleName, String lastName, String prefName){
+		
+		// Constructor variable names are different from constructor input names to prevent variable shadowing
+		
+	}
+	
+}
