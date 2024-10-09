915def14ef1b6ec23da6b0ff2821efff569433bc
snesan0821 <snesan@asu.edu>
2024-10-08 21:25:51 -0700

    SelectRoleClass Updates
    finished this too, still need to add the accountinformation so that the buttons appear depending on user.

--- src/application/SelectRole.java
+++ src/application/SelectRole.java
@@ -11,36 +11,42 @@ import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-public class SelectRole {
+public class SelectRole extends Application {
 
-    //previous logic: already have determined user role
-    private String userRole = "Student"; //hard-coded for now, could be "Instructor" or "Admin"
+    // previous logic: already have determined user role
+    private String userRole = "Student"; // hard-coded for now
+    // we will replace this with the logic to see if the user is using the accountdatabase
     
-    
-    
-    /* implement using a construotr that calls that method
-     * pass in the username, Pane
-     * check username role
-     */
+    // Instantiate 
+    public SetupUIElements setupUI = new SetupUIElements();
 
+    @Override
     public void start(Stage primaryStage) {
-        // Title Text
-        Text title = new Text("Select Role");
-
-        // Role Buttons
+    	// Create "Select Role" text
+    	Text title = new Text("Select Role");
+    	title.setFont(new Font("Arial", 32));  // Set font
+    	title.setFill(Color.BLACK);  // Set the text
+    	
+        // Create Role Buttons
         Button studentButton = new Button("Student");
         Button instructorButton = new Button("Instructor");
         Button adminButton = new Button("Admin");
 
-        // Call the method to disable buttons based on the user's role
+        // Set up the buttons
+        setupUI.SetupButtonUI(studentButton, "Arial", 14, 200, Pos.CENTER, 0, 0, false, Color.BLACK);
+        setupUI.SetupButtonUI(instructorButton, "Arial", 14, 200, Pos.CENTER, 0, 0, false, Color.BLACK);
+        setupUI.SetupButtonUI(adminButton, "Arial", 14, 200, Pos.CENTER, 0, 0, false, Color.BLACK);
+
+        // disable buttons based on the user's role
         setButtonAccess(studentButton, instructorButton, adminButton);
 
         // VBox Layout
-        VBox vbox = new VBox(10); // Spacing of 10 between elements
+        VBox vbox = new VBox(10); // Spacing of 10
+        vbox.setAlignment(Pos.CENTER); //center
         vbox.getChildren().addAll(title, studentButton, instructorButton, adminButton);
 
         // Scene
-        Scene scene = new Scene(vbox, 300, 200); 
+        Scene scene = new Scene(vbox, 300, 200);
 
         // Primary Stage
         primaryStage.setTitle("Select Role");
@@ -49,7 +55,7 @@ public class SelectRole {
     }
 
     /**
-     * button access (enable/disable) based on the user's role
+     * Set button access (enable/disable) based on the user's role
      */
     private void setButtonAccess(Button studentBtn, Button instructorBtn, Button adminBtn) {
         switch (userRole) {
@@ -59,24 +65,16 @@ public class SelectRole {
                 adminBtn.setDisable(true); // Disable "Admin" button
                 break;
             case "Instructor":
-                // An instructor can access "Instructor" and "Student" options, but not "Admin"
+                // An instructor can access "Instructor" and "Student" options,not "Admin"
                 adminBtn.setDisable(true); // Disable "Admin" button
                 break;
             case "Admin":
-                // No action needed since all buttons are enabled by default
+                // No action
                 break;
-          /*  default:
-                studentBtn.setDisable(true);
-                instructorBtn.setDisable(true);
-                adminBtn.setDisable(true);
-                System.out.println("Invalid role: access denied");
-                */
         }
     }
 
     public static void main(String[] args) {
         launch(args);
     }
-
-
 }
