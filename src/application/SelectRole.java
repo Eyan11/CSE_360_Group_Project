73035eecd317c286873a6c098f3d2303f8eb88b7
snesan0821 <snesan@asu.edu>
2024-10-08 21:15:04 -0700

    bin stuff updating

--- src/application/SelectRole.java
+++ src/application/SelectRole.java
@@ -1,9 +1,13 @@
 package application;
 
-
+import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
