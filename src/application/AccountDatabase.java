1092909b73e28a1957df1b6c76d674ac67572d01
Eyan <eyanmartucci11@gmail.com>
2024-10-09 20:46:04 -0700

    Progress on DatabaseTestingAutomation
    - Added a testing method for every method inside of AccountDatabase with documentation
    - Started writing test cases, still in progress

--- src/application/AccountDatabase.java
+++ src/application/AccountDatabase.java
@@ -8,7 +8,7 @@ import java.util.Random; // For random key generator
  * 
  * <p> Description: Manages the H2 database which stores all account information.</p>
  * 
- * <p> Source: Lynn Rober Carter from FirstDatabase project, DatabaseHelper class, 
+ * <p> Source: Lynn Robert Carter from FirstDatabase project, DatabaseHelper class, 
  * 				available at: https://canvas.asu.edu/courses/193728/files/92728837?module_item_id=14758007
  * 
  * @author Eyan Martucci
@@ -34,14 +34,14 @@ public class AccountDatabase {
 	private static ResultSet resultSet = null;
 	
 	// For random key generator
-	private final String KEY_CHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_#$@";
-	private final int KEY_LENGTH = 15;
-	private Random random;
+	private static final String KEY_CHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_#$@";
+	private static final int KEY_LENGTH = 15;
+	private static Random random;
 	
 	
 	/**********************************************************************************************
 
-	Private Methods For Database Connection
+	Public Methods For Database Connection
 	
 	**********************************************************************************************/
 	
@@ -297,21 +297,22 @@ public class AccountDatabase {
 	 * Creates the first account in accounts database with provided username and password.
 	 * If there is already an account, it will exit the method and print an error message.
 	 */
-	public static void createFirstAccount(String user, String pass) throws SQLException {
+	public static boolean createFirstAccount(String user, String pass) throws SQLException {
 		
 		// Prevents using method when not the first account in database
 		if(!isDatabaseEmpty()) {
 			System.err.println("Can't create first account, database is NOT empty");
+			return false;
 		}
 		// Prevents very long usernames
 		if(user.length() > 50) {
 			System.err.println("Cannot create account username is over 50 characters");
-			return;
+			return false;
 		}
 		// Prevents very long passwords
 		if(pass.length() > 50) {
 			System.err.println("Cannot create account password is over 50 characters");
-			return;
+			return false;
 		}
 		
 		
@@ -332,10 +333,14 @@ public class AccountDatabase {
 		}
 		
 		// Print results
-		if(doesLoginExist(user, pass))
+		if(doesLoginExist(user, pass)) {
 			System.out.println("First user and pass successfully inserted into accounts database!");
-		else 
+			return true;
+		}
+		else {
 			System.err.println("First user and pass failed to be inserted into database");
+			return false;
+		}
 	}
 
 	
@@ -343,22 +348,22 @@ public class AccountDatabase {
 	 * Updates account username and password to the account corresponding to the given key in database.
 	 * If key doesn't exist in database, or username already exists, it will exit with error message.
 	 */
-	public static void createAccountWithKey(String user, String pass, String key) throws SQLException {
+	public static boolean createAccountWithKey(String user, String pass, String key) throws SQLException {
 		
 		// Prevents duplicate usernames
 		if(doesUsernameExist(user)) {
 			System.err.println("Cannot create account because username already exists");
-			return;
+			return false;
 		}
 		// Prevents very long usernames
 		if(user.length() > 50) {
 			System.err.println("Cannot create account username is over 50 characters");
-			return;
+			return false;
 		}
 		// Prevents very long passwords
 		if(pass.length() > 50) {
 			System.err.println("Cannot create account password is over 50 characters");
-			return;
+			return false;
 		}
 		
 		
@@ -379,10 +384,14 @@ public class AccountDatabase {
 		}
 		
 		// Print results
-		if(doesLoginExist(user, pass))
+		if(doesLoginExist(user, pass)) {
 			System.out.println("New user and pass successfully inserted into accounts database!");
-		else 
+			return true;
+		}
+		else {
 			System.err.println("New user and pass failed to be inserted into database");
+			return false;
+		}
 	}
 
 	
@@ -390,43 +399,43 @@ public class AccountDatabase {
 	 * Updates additional account information to the account corresponding to the given username in database.
 	 * If username doesn't exist, or email already exists, it will exit and print an error message.
 	 */
-	public static void updateAccountInformation(String user, String email, String firstName, String middleName, 
+	public static boolean updateAccountInformation(String user, String email, String firstName, String middleName, 
 			String lastName, String prefName) throws SQLException{
 		
 		// Checks if username doesn't exist
 		if(!doesUsernameExist(user)) {
 			System.err.println("Cannot update account, username does not exist in accounts database");
-			return;
+			return false;
 		}
 		// Checks if email exists
 		if(doesEmailExist(email)) {
 			System.err.println("Cannot update account, email already exists in database");
-			return;
+			return false;
 		}
 		// Prevents very long emails
 		if(email.length() > 50) {
 			System.err.println("Cannot update account, email is over 50 characters");
-			return;
+			return false;
 		}
 		// Prevents very long first names
 		if(firstName.length() > 50) {
 			System.err.println("Cannot update account, first name is over 50 characters");
-			return;
+			return false;
 		}
 		// Prevents very long middle names
 		if(middleName.length() > 50) {
 			System.err.println("Cannot update account, middle name is over 50 characters");
-			return;
+			return false;
 		}
 		// Prevents very long last names
 		if(lastName.length() > 50) {
 			System.err.println("Cannot update account, last name is over 50 characters");
-			return;
+			return false;
 		}
 		// Prevents very long preferred names
 		if(prefName != null && prefName.length() > 50) {
 			System.err.println("Cannot update account, preferred name is over 50 characters");
-			return;
+			return false;
 		}
 		
 		String query;
@@ -466,15 +475,29 @@ public class AccountDatabase {
 			}
 			
 			pstmt.executeUpdate();						// execute query
+			System.out.println("Successfully updated " + user + "'s account");
+			return true;
 		}
-		System.out.println("Successfully updated " + user + "'s account");
 	}
 	
 	
 	/**********
 	 * Adds a random unique key and user roles into database, then returns the key.
+	 * Returning an empty string means inviteUser failed.
 	 */
-	public String inviteUser(boolean isStudent, boolean isInstructor, boolean isAdmin) throws SQLException{
+	public static String inviteUser(boolean isStudent, boolean isInstructor, boolean isAdmin) throws SQLException{
+		
+		// Prevent an account from having no roles
+		if(!isStudent && !isInstructor && !isAdmin) {
+			System.err.println("Cannot invite user because new user must have at least 1 role");
+			return "";
+		}
+		
+		// Prevent an account from having BOTH student and instructor roles
+		if(isStudent && isInstructor) {
+			System.err.println("Cannot invite user because new user cannot have BOTH student and instructor role");
+			return "";
+		}
 		
 		// generate random unique key
 		String key = generateKey();
@@ -510,14 +533,14 @@ public class AccountDatabase {
 	
 	/**********************************************************************************************
 
-	Private Methods
+	 Public Methods To Create/Delete 'accounts' Table
 	
 	**********************************************************************************************/
 	
 	/**********
 	 * Creates a table called accounts and initializes columns.
 	 */
-	private static void createTable() throws SQLException {
+	public static void createTable() throws SQLException {
 		query = "CREATE TABLE IF NOT EXISTS accounts ("
 				+ "username VARCHAR(50) PRIMARY KEY UNIQUE, "
 				+ "password VARCHAR(50), "		// VARCHAR(#) is a string with a max of # characters
@@ -541,17 +564,24 @@ public class AccountDatabase {
 	/**********
 	 * Deletes the entire accounts table in database
 	 */
-	private void deleteTable() throws SQLException {
+	public static void deleteTable() throws SQLException {
 		query = "DROP TABLE accounts";		// delete accounts table
 		statement.execute(query);			// execute query
 		System.out.print("'accounts' table deleted");
 	}
 	
 	
+	/**********************************************************************************************
+
+	 Private Helper Methods
+	
+	**********************************************************************************************/
+	
+	
 	/**********
 	 * Generates a random key with a-z, A-z, and 0-9 characters of length 15
 	 */
-	private String generateKey() throws SQLException{
+	private static String generateKey() throws SQLException{
 		String newKey = "";
 		random = new Random();
 		int randValue;
@@ -574,116 +604,4 @@ public class AccountDatabase {
 		return newKey;	// successfully generated unique key
 	}
 	
-	
-	/**********
-	 * Deletes the entire accounts table in database
-	 */
-	private void testDatabase() throws SQLException {
-		deleteTable();
-		createTable();
-		createFirstAccount("First user", "123");	// test first account creation
-		createFirstAccount("Second account", "123");
-		
-		//createAccountTest("Bob", "123", "Bob@gmail.com", true, false, false, false);
-	}
-	
-	
-	/**********
-	 * Creates a new account in account database for testing purposes.
-	 */
-	private void createAccountTest(String user, String pass, String email, 
-			boolean isStudent, boolean isInstructor, boolean isAdmin, boolean isKey) throws SQLException {
-		
-		// Prevents duplicate usernames
-		if(doesUsernameExist(user)) {
-			System.err.println("Cannot create account because username already exists");
-			return;
-		}
-		// Prevents duplicate emails
-		if(doesEmailExist(email)) {
-			System.err.println("Cannot create account because email already exists");
-			return;
-		}
-		// Prevents very long usernames
-		if(user.length() > 50) {
-			System.err.println("Cannot create account username is over 50 characters");
-			return;
-		}
-		// Prevents very long passwords
-		if(pass.length() > 50) {
-			System.err.println("Cannot create account password is over 50 characters");
-			return;
-		}
-		// Prevents very long emails
-		if(email.length() > 50) {
-			System.err.println("Cannot create account email is over 50 characters");
-			return;
-		}
-			
-		// Query creates a new row in database with provided parameters
-		query = "INSERT INTO accounts (username, password, email, is_student, is_instructor, is_admin, is_key) VALUES (?, ?, ?, ?, ?, ?, ?)";
-		try (PreparedStatement pstmt = connection.prepareStatement(query)) {
-			pstmt.setString(1, user);
-			pstmt.setString(2, pass);
-			pstmt.setString(3, email);
-			
-			if(isStudent) pstmt.setInt(4, 1);		// is_student = true
-			else pstmt.setInt(4, 0);				// is_student = false
-			
-			if(isInstructor) pstmt.setInt(5, 1);	// is_instructor = true
-			else pstmt.setInt(5, 0);				// is_instructor = false
-			
-			if(isAdmin) pstmt.setInt(6, 1);			// is_admin = true
-			else pstmt.setInt(6, 0);				// is_admin = false
-			
-			if(isKey) pstmt.setInt(7, 1);			// is_key = true
-			else pstmt.setInt(7, 0);				// is_key = false
-			
-			pstmt.executeUpdate();					// execute query
-		}
-		System.out.println("Created test account for '" + user + "'");
-	}
-	
-	
-	/**********
-	 * Prints all account information for all accounts in account database for testing purposes.
-	 */
-	private void printAccountsTest() throws SQLException{
-		// get entire accounts table
-		query = "SELECT * FROM accounts"; 
-		statement = connection.createStatement();
-		resultSet = statement.executeQuery(query); 
-		
-		System.out.println("----------------------------------------------------");
-		System.out.println("Printing All Accounts: \n");
-		System.out.println("----------------------------------------------------");
-
-		// while the row exists
-		while(resultSet.next()) { 
-			// Retrieve by column name 
-			String user = resultSet.getString("username"); 
-			String pass = resultSet.getString("password"); 
-			String email = resultSet.getString("email"); 
-			String fName = resultSet.getString("first_name");
-			String mName = resultSet.getString("middle_name");
-			String lName = resultSet.getString("last_name");
-			String pName = resultSet.getString("preferred_name");
-			String dName = resultSet.getString("display_name");
-			int isKey = resultSet.getInt("is_key"); 
-
-			// Display values 
-			System.out.println("Account: ");
-			System.out.println("Username: " + user); 
-			System.out.println("Password: " + pass); 
-			System.out.println("Email: " + email); 
-			System.out.println("First Name: " + fName); 
-			System.out.println("Middle Name: " + mName); 
-			System.out.println("Last Name: " + lName); 
-			System.out.println("Preferred Name: " + pName); 
-			System.out.println("Display Name: " + dName); 
-			System.out.println("isKey: " + isKey); 
-			System.out.println("----------------------------------------------------\n");
-		} 
-	}
-
 }
--- src/application/DatabaseTestingAutomation.java
+++ src/application/DatabaseTestingAutomation.java
@@ -1,7 +1,460 @@
 package application;
 
+import java.sql.*; // For SQL related objects
+
 public class DatabaseTestingAutomation {
 
 	
+	// Reusable variables to communicate with database
+	private static String query = "";
+	private static Connection connection = null;
+	private static Statement statement = null; 
+	private static ResultSet resultSet = null;
 	
+	private boolean actualResult;
+	/** Total number of test cases that had matching expectations and results */
+	private static int numPassed = 0;
+	/** Total number of test cases that had different expectations and results */
+	private static int numFailed = 0;
+	
+	private String[] keyArr = new String[3];
+	private int i = 0;
+	private String key = "";
+	
+	
+	/**********
+	 * Starts the testing automatotion
+	 */
+	private void performTestEvaluations() throws SQLException {
+		// Reset accounts table
+		AccountDatabase.deleteTable();
+		AccountDatabase.createTable();
+		
+		// *** Test createFirstAccount() ***********************************
+		testCreateFirstAccount("First user", "123", true);
+		testCreateFirstAccount("Second account", "123", false);
+		// *****************************************************************
+		
+		// *** Test inviteUser() *******************************************
+		testInviteUser(true, false, false, true);
+		testInviteUser(false, false, true, true);
+		testInviteUser(false, true, true, true);
+		testInviteUser(false, false, false, false);
+		testInviteUser(true, true, false, false);
+		testInviteUser(true, true, true, false);
+		// *****************************************************************
+		
+		// *** Test doesKeyExist() *****************************************
+		testDoesKeyExist(keyArr[0], true);
+		testDoesKeyExist(keyArr[1], true);
+		testDoesKeyExist("", false);
+		testDoesKeyExist("crmc4324c7y23m4c087234SEKFJ", false);
+		// *****************************************************************
+		
+		// *** Test createAccountWithKey() *********************************
+		testCreateAccountWithKey("Name1", "123", keyArr[0], true);
+		testCreateAccountWithKey("Name2", "123", keyArr[1], true);
+		testCreateAccountWithKey("NotAdded", "123", keyArr[1], true);
+		// *****************************************************************
+		
+		
+		// Print Results
+		System.out.println("-----------------------------------------------------------");
+		System.out.println("\n\n\n Restults: ");
+		System.out.println("Number of tests that passed: " + numPassed);
+		System.out.println("Number of tests that failed" + numFailed);
+	}
+	
+	
+	/**********************************************************************************************
+
+	Private Helper Methods To Test All Functionalities of AccountDatabase Class
+	
+	**********************************************************************************************/
+	
+	
+	/**********
+	 * Tests the functionality of the createFirstAccount() method in AccountDatabase class.
+	 */
+	private void testCreateFirstAccount(String user, String pass, boolean expectedResult) throws SQLException{
+		
+		// Attempt to create first account and get result
+		actualResult = AccountDatabase.createFirstAccount(user, pass);
+		
+		// Return if test passed or failed and track
+		if(actualResult == expectedResult) {
+			numPassed++;
+			System.out.println("createFirstAccount() passed!");
+		}
+		else {
+			numFailed++;
+			System.out.println("createFirstAccount() failed!");
+		}
+	}
+	
+	
+	/**********
+	 * Tests the functionality of the createAccountWithKey() method in AccountDatabase class.
+	 */
+	private void testCreateAccountWithKey(String user, String pass, String key, boolean expectedResult) throws SQLException{
+		
+		// Attempt to create account with a key and get result
+		actualResult = AccountDatabase.createAccountWithKey(user, pass, key);
+		
+		// Return if test passed or failed and track
+		if(actualResult == expectedResult) {
+			numPassed++;
+			System.out.println("createAccountWithKey() passed!");
+		}
+		else {
+			numFailed++;
+			System.out.println("createAccountWithKey() failed!");
+		}
+	}
+	
+	
+	/**********
+	 * Tests the functionality of the updateAccountInformation() method in AccountDatabase class.
+	 */
+	private void testUpdateAccountInformation(String user, String email, String firstName, String middleName, 
+			String lastName, String prefName, boolean expectedResult) throws SQLException{
+		
+		// Attempt to update account information
+		actualResult = AccountDatabase.updateAccountInformation(user, email, firstName, middleName, lastName, prefName);
+		
+		// Return if test passed or failed and track
+		if(actualResult == expectedResult) {
+			numPassed++;
+			System.out.println("testUpdateAccountInformation() passed!");
+		}
+		else {
+			numFailed++;
+			System.out.println("testUpdateAccountInformation() failed!");
+		}
+	}
+	
+
+	/**********
+	 * Tests the functionality of the inviteUser() method in AccountDatabase class.
+	 */
+	private void testInviteUser(boolean isStudent, boolean isInstructor, boolean isAdmin, boolean expectedResult) throws SQLException{
+		
+		// Attempt to invite user and use return key
+		key = AccountDatabase.inviteUser(isStudent, isInstructor, isAdmin);
+		
+		// If no key then invite failed
+		if(key == "")
+			actualResult = false;
+		// If key then invite succeeded
+		else
+			actualResult = true;
+		
+		// Return if test passed or failed and track
+		if(actualResult == expectedResult) {
+			numPassed++;
+			System.out.println("inviteUser() passed!");
+			// Store key for future test cases
+			keyArr[i] = key;
+			i++;
+		}
+		else {
+			numFailed++;
+			System.out.println("inviteUser() failed!");
+		}
+	}
+	
+	
+	/**********
+	 * Tests the functionality of the isDatabaseEmpty() method in AccountDatabase class.
+	 */
+	private void testIsDatabaseEmpty(boolean expectedResult) throws SQLException{
+		
+		// Check if database is empty and return result
+		actualResult = AccountDatabase.isDatabaseEmpty();
+		
+		// Return if test passed or failed and track
+		if(actualResult == expectedResult) {
+			numPassed++;
+			System.out.println("isDatabaseEmpty() passed!");
+		}
+		else {
+			numFailed++;
+			System.out.println("isDatabaseEmpty() failed!");
+		}
+	}
+	
+	
+	/**********
+	 * Tests the functionality of the doesLoginExist() method in AccountDatabase class.
+	 */
+	private void testDoesLoginExist(String user, String pass, boolean expectedResult) throws SQLException{
+		
+		// Check if login exists and return result
+		actualResult = AccountDatabase.doesLoginExist(user, pass);
+		
+		// Return if test passed or failed and track
+		if(actualResult == expectedResult) {
+			numPassed++;
+			System.out.println("doesLoginExist() passed!");
+		}
+		else {
+			numFailed++;
+			System.out.println("doesLoginExist() failed!");
+		}
+	}
+	
+	
+	/**********
+	 * Tests the functionality of the doesKeyExist() method in AccountDatabase class.
+	 */
+	private void testDoesKeyExist(String key, boolean expectedResult) throws SQLException{
+		
+		// Check if key exists and return result
+		actualResult = AccountDatabase.doesKeyExist(key);
+		
+		// Return if test passed or failed and track
+		if(actualResult == expectedResult) {
+			numPassed++;
+			System.out.println("doesKeyExist() passed!");
+		}
+		else {
+			numFailed++;
+			System.out.println("doesKeyExist() failed!");
+		}
+	}
+	
+	
+	/**********
+	 * Tests the functionality of the doesUsernameExist() method in AccountDatabase class.
+	 */
+	private void testDoesUsernameExist(String user, boolean expectedResult) throws SQLException{
+		
+		// Check if username exists and return result
+		actualResult = AccountDatabase.doesUsernameExist(user);
+		
+		// Return if test passed or failed and track
+		if(actualResult == expectedResult) {
+			numPassed++;
+			System.out.println("doesUsernameExist() passed!");
+		}
+		else {
+			numFailed++;
+			System.out.println("doesUsernameExist() failed!");
+		}
+	}
+	
+	
+	/**********
+	 * Tests the functionality of the doesEmailExist() method in AccountDatabase class.
+	 */
+	private void testDoesEmailExist(String email, boolean expectedResult) throws SQLException{
+		
+		// Check if email exists and return result
+		actualResult = AccountDatabase.doesEmailExist(email);
+		
+		// Return if test passed or failed and track
+		if(actualResult == expectedResult) {
+			numPassed++;
+			System.out.println("doesEmailExist() passed!");
+		}
+		else {
+			numFailed++;
+			System.out.println("doesEmailExist() failed!");
+		}
+	}
+	
+
+	/**********
+	 * Tests the functionality of the isStudentRole() method in AccountDatabase class.
+	 */
+	private void testIsStudentRole(String user, boolean expectedResult) throws SQLException{
+		
+		// Check user has student role and return result
+		actualResult = AccountDatabase.isStudentRole(user);
+		
+		// Return if test passed or failed and track
+		if(actualResult == expectedResult) {
+			numPassed++;
+			System.out.println("isStudentRole() passed!");
+		}
+		else {
+			numFailed++;
+			System.out.println("isStudentRole() failed!");
+		}
+	}
+	
+	
+	/**********
+	 * Tests the functionality of the isInstructorRole() method in AccountDatabase class.
+	 */
+	private void testIsInstructorRole(String user, boolean expectedResult) throws SQLException{
+		
+		// Check user has instructor role and return result
+		actualResult = AccountDatabase.isInstructorRole(user);
+		
+		// Return if test passed or failed and track
+		if(actualResult == expectedResult) {
+			numPassed++;
+			System.out.println("isInstructorRole() passed!");
+		}
+		else {
+			numFailed++;
+			System.out.println("isInstructorRole() failed!");
+		}
+	}
+	
+	
+	/**********
+	 * Tests the functionality of the isAdmin() method in AccountDatabase class.
+	 */
+	private void testIsAdmin(String user, boolean expectedResult) throws SQLException{
+		
+		// Check user has admin role and return result
+		actualResult = AccountDatabase.isAdminRole(user);
+		
+		// Return if test passed or failed and track
+		if(actualResult == expectedResult) {
+			numPassed++;
+			System.out.println("isAdminRole() passed!");
+		}
+		else {
+			numFailed++;
+			System.out.println("isAdminRole() failed!");
+		}
+	}
+	
+	
+	/**********
+	 * Tests the functionality of the isAccountUpdated() method in AccountDatabase class.
+	 */
+	private void testIsAccountUpdated(String user, boolean expectedResult) throws SQLException{
+		
+		// Check account has updated information and return result
+		actualResult = AccountDatabase.isAccountUpdated(user);
+		
+		// Return if test passed or failed and track
+		if(actualResult == expectedResult) {
+			numPassed++;
+			System.out.println("isAccountUpdated() passed!");
+		}
+		else {
+			numFailed++;
+			System.out.println("isAccountUpdated() failed!");
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/**********
+	 * Creates a new account in account database for testing purposes.
+	 */
+	private void createAccountTest(String user, String pass, String email, 
+			boolean isStudent, boolean isInstructor, boolean isAdmin, boolean isKey) throws SQLException {
+		
+		// Prevents duplicate usernames
+		if(AccountDatabase.doesUsernameExist(user)) {
+			System.err.println("Cannot create account because username already exists");
+			return;
+		}
+		// Prevents duplicate emails
+		if(AccountDatabase.doesEmailExist(email)) {
+			System.err.println("Cannot create account because email already exists");
+			return;
+		}
+		// Prevents very long usernames
+		if(user.length() > 50) {
+			System.err.println("Cannot create account username is over 50 characters");
+			return;
+		}
+		// Prevents very long passwords
+		if(pass.length() > 50) {
+			System.err.println("Cannot create account password is over 50 characters");
+			return;
+		}
+		// Prevents very long emails
+		if(email.length() > 50) {
+			System.err.println("Cannot create account email is over 50 characters");
+			return;
+		}
+			
+		// Query creates a new row in database with provided parameters
+		query = "INSERT INTO accounts (username, password, email, is_student, is_instructor, is_admin, is_key) VALUES (?, ?, ?, ?, ?, ?, ?)";
+		try (PreparedStatement pstmt = connection.prepareStatement(query)) {
+			pstmt.setString(1, user);
+			pstmt.setString(2, pass);
+			pstmt.setString(3, email);
+			
+			if(isStudent) pstmt.setInt(4, 1);		// is_student = true
+			else pstmt.setInt(4, 0);				// is_student = false
+			
+			if(isInstructor) pstmt.setInt(5, 1);	// is_instructor = true
+			else pstmt.setInt(5, 0);				// is_instructor = false
+			
+			if(isAdmin) pstmt.setInt(6, 1);			// is_admin = true
+			else pstmt.setInt(6, 0);				// is_admin = false
+			
+			if(isKey) pstmt.setInt(7, 1);			// is_key = true
+			else pstmt.setInt(7, 0);				// is_key = false
+			
+			pstmt.executeUpdate();					// execute query
+		}
+		System.out.println("Created test account for '" + user + "'");
+	}
+	
+	
+	/**********
+	 * Prints all account information for all accounts in account database for testing purposes.
+	 */
+	private void printAccountsTest() throws SQLException{
+		// get entire accounts table
+		query = "SELECT * FROM accounts"; 
+		statement = connection.createStatement();
+		resultSet = statement.executeQuery(query); 
+		
+		System.out.println("----------------------------------------------------");
+		System.out.println("Printing All Accounts: \n");
+		System.out.println("----------------------------------------------------");
+
+		// while the row exists
+		while(resultSet.next()) { 
+			// Retrieve by column name 
+			String user = resultSet.getString("username"); 
+			String pass = resultSet.getString("password"); 
+			String email = resultSet.getString("email"); 
+			String fName = resultSet.getString("first_name");
+			String mName = resultSet.getString("middle_name");
+			String lName = resultSet.getString("last_name");
+			String pName = resultSet.getString("preferred_name");
+			String dName = resultSet.getString("display_name");
+			int isKey = resultSet.getInt("is_key"); 
+
+			// Display values 
+			System.out.println("Account: ");
+			System.out.println("Username: " + user); 
+			System.out.println("Password: " + pass); 
+			System.out.println("Email: " + email); 
+			System.out.println("First Name: " + fName); 
+			System.out.println("Middle Name: " + mName); 
+			System.out.println("Last Name: " + lName); 
+			System.out.println("Preferred Name: " + pName); 
+			System.out.println("Display Name: " + dName); 
+			System.out.println("isKey: " + isKey); 
+			System.out.println("----------------------------------------------------\n");
+		} 
+	}
 }
