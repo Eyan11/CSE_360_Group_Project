74c36c4fd328b99939d42d8d9e12b5741842aa17
juliosalazarr <jasala15@asu.edu>
2024-10-28 11:22:37 -0700

    Button Functionality for Backup and Restore
    - Added button Functionalit for both BackupArticlesGUI and RestoreArticlesGUI
    - Just waiting on ManageArticlesGUI for both classes to finalize each
    - Added documentation to both classes as well

--- src/application/BackupArticlesGUI.java
+++ src/application/BackupArticlesGUI.java
@@ -13,19 +13,23 @@ import javafx.event.EventHandler; // For EventHandler object
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 
+import java.io.IOException;
+
 //import database.AccountDatabase;
 
 import java.sql.*;
 
+import database.ArticleDatabase;
+
 /*******
  * <p> BackupArticlesGUI Class </p>
  * 
- * <p> Description: ... </p>
+ * <p> Description: User Interface for Backing-Up Articles </p>
  * 
  * @author Julio Salazar
  * 
  * @version 1.00 10/26/2024 Phase 2 Implementation and Documentation
- * 
+ * 			1.25 10/28/2024 Added Button Functionality (waiting on ManageArticlesGUI)
  */
 
 public class BackupArticlesGUI
@@ -38,20 +42,20 @@ public class BackupArticlesGUI
 	public final static double WINDOW_WIDTH = 500;
 	public final static double WINDOW_HEIGHT = 430;
 	
-	//
+	//store inputed file path and group from user
 	private String filePathInput = "";
 	private String groupInput = "";
 	
-	//
+	//displays instructions for user input
 	private Label filePathLabel = new Label("File Path: ");
 	private Label instructionLabel = new Label("(leave empty for all groups)");
 	private Label groupLabel = new Label("Groups: ");
 	
-	//text field for user input
+	//text fields for user input
 	private TextField filePathText = new TextField();
 	private TextField groupText = new TextField();
 	
-	//
+	//buttons used to navigate interface and backing-up
 	private Button backButton = new Button("<-");
 	private Button backupButton = new Button("Backup");
 	
@@ -60,7 +64,7 @@ public class BackupArticlesGUI
 	
 	/**
 	 * Constructor w/ Parameter for GUI
-	 * @param userPane
+	 * @param theRoot
 	 */
 	
 	public BackupArticlesGUI(Pane theRoot) throws SQLException
@@ -72,15 +76,15 @@ public class BackupArticlesGUI
 		 * Label Creations
 		 */
 		
-		//
+		//Label asking user for file path
 		setupUI.SetupLabelUI(filePathLabel, "Arial", 36, WINDOW_WIDTH-10, 
 				Pos.BASELINE_LEFT, 10, 50, Color.BLACK);
 		
-		//
+		//Label letting user result of leaving group field left unfilled
 		setupUI.SetupLabelUI(instructionLabel, "Arial", 18, WINDOW_WIDTH-10, 
 				Pos.BASELINE_LEFT, 175, 63, Color.BLACK);
 		
-		//
+		//Label asking user for group(s) they would like to back-up
 		setupUI.SetupLabelUI(groupLabel, "Arial", 36, WINDOW_WIDTH-10, 
 				Pos.BASELINE_LEFT, 10, 200, Color.BLACK);
 		
@@ -88,11 +92,11 @@ public class BackupArticlesGUI
 		 * TextField Creations 
 		 */
 		
-		//
+		//Text field that stores user's file path for back-up
 		setupUI.SetupTextFieldUI(filePathText, "Arial", 18, 400, 40,
 				Pos.BASELINE_LEFT, 10, 110, true);
 		
-		//
+		//Text field that stores user's group(s) for back-up
 		setupUI.SetupTextFieldUI(groupText, "Arial", 18, 400, 40,
 				Pos.BASELINE_LEFT, 10, 260, true);
 		
@@ -100,11 +104,11 @@ public class BackupArticlesGUI
 		 * Button Creations
 		 */
 		
-		//Button that ...
+		//Button that returns user to previous interface
 		setupUI.SetupButtonUI(backButton, "Arial", 11, 50, 20,
         		Pos.CENTER, 10, 10, false, Color.BLACK);
 		
-		//Button that ...
+		//Button that backs-up articles specified by user by group(s)
 		setupUI.SetupButtonUI(backupButton, "Arial", 18, 200, 50,
         		Pos.CENTER, 150, 325, false, Color.BLACK);
 		
@@ -125,17 +129,14 @@ public class BackupArticlesGUI
 		{
 			public void handle(ActionEvent event) 
 			{						
-				/*
-				 * 
-				 */
-				
+				//returns user back to previous page
 				theRoot.getChildren().clear();  // Clear the current root
-				
-				Pane newRoot = new Pane(); // 
-				//ManageArticlesGUI manageArticles = new ManageArticlesGUI(theRoot); //
-				Scene newScene = new Scene(newRoot, WINDOW_WIDTH, WINDOW_HEIGHT); //
-			    Stage currentStage = (Stage) theRoot.getScene().getWindow(); // 
-			    currentStage.setScene(newScene); // 
+				//create new pane for next interface
+				Pane newRoot = new Pane();
+				//ManageArticlesGUI manageArticles = new ManageArticlesGUI(theRoot); // Returns user to previous interface
+				Scene newScene = new Scene(newRoot, WINDOW_WIDTH, WINDOW_HEIGHT); // creates new scene
+			    Stage currentStage = (Stage) theRoot.getScene().getWindow();
+			    currentStage.setScene(newScene); // sets new scene
 			}
 		});
 		
@@ -147,17 +148,37 @@ public class BackupArticlesGUI
 		{
 			public void handle(ActionEvent event) 
 			{	
-				/*
-				 * 
-				 */
+				//retrieves file path from user
+				filePathInput = filePathText.getText();
+				//retrieves group(s) from user
+				groupInput = groupText.getText();
 				
-				theRoot.getChildren().clear();  // Clear the current root
-				
-				Pane newRoot = new Pane(); // 
-				//ManageArticlesGUI manageArticles = new ManageArticlesGUI(theRoot); //
-				Scene newScene = new Scene(newRoot, WINDOW_WIDTH, WINDOW_HEIGHT); //
-			    Stage currentStage = (Stage) theRoot.getScene().getWindow(); // 
-			    currentStage.setScene(newScene); // 
+				//try and catch block needed for exception handling
+				try {
+					//Back-up Articles
+					if(ArticleDatabase.backupArticles(filePathInput, groupInput))
+					{
+						//returns user back to previous page
+						theRoot.getChildren().clear();  // Clear the current root
+						//create new pane for next interface
+						Pane newRoot = new Pane();
+						//ManageArticlesGUI manageArticles = new ManageArticlesGUI(theRoot); // Returns user to previous interface
+						Scene newScene = new Scene(newRoot, WINDOW_WIDTH, WINDOW_HEIGHT); // creates new scene
+					    Stage currentStage = (Stage) theRoot.getScene().getWindow();
+					    currentStage.setScene(newScene); // sets new scene
+					}
+					else // Back-Up Failed
+					{
+						//print to console letting user know back-up failed
+						System.out.println("Backup Failed!");
+					}
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
 		});
 	}
--- src/application/RestoreArticlesGUI.java
+++ src/application/RestoreArticlesGUI.java
@@ -13,19 +13,23 @@ import javafx.event.EventHandler; // For EventHandler object
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 
+import java.io.IOException;
+
 //import database.AccountDatabase;
 
 import java.sql.*;
 
+import database.ArticleDatabase;
+
 /*******
  * <p> BackupArticlesGUI Class </p>
  * 
- * <p> Description: ... </p>
+ * <p> Description: User Interface for Article Restoration </p>
  * 
  * @author Julio Salazar
  * 
  * @version 1.00 10/26/2024 Phase 2 Implementation and Documentation
- * 
+ * 			1.25 10/28/2024 Added Button Functionality (waiting on ManageArticlesGUI)
  */
 
 public class RestoreArticlesGUI
@@ -34,20 +38,20 @@ public class RestoreArticlesGUI
 	 * Variable Declarations
 	 */
 
-	//The width/height of the pop-up window for the user interface
+	//the width/height of the pop-up window for the user interface
 	public final static double WINDOW_WIDTH = 500;
 	public final static double WINDOW_HEIGHT = 430;
 	
-	//
+	//will store inputed file path for restoration
 	private String filePathInput = "";
 	
-	//
+	//asks user to input a file path
 	private Label filePathLabel = new Label("File Path: ");
 	
-	//text field for user input
+	//text field for user file path
 	private TextField filePathText = new TextField();
 	
-	//
+	//buttons used to navigate interface and restoration
 	private Button backButton = new Button("<-");
 	private Button overrideButton = new Button("Restore by Overriding");
 	private Button mergeButton = new Button("Restore by Merging");
@@ -57,7 +61,7 @@ public class RestoreArticlesGUI
 	
 	/**
 	 * Constructor w/ Parameter for GUI
-	 * @param userPane
+	 * @param theRoot
 	 */
 	
 	public RestoreArticlesGUI(Pane theRoot) throws SQLException
@@ -69,7 +73,7 @@ public class RestoreArticlesGUI
 		 * Label Creations
 		 */
 		
-		//
+		//Label asking user for File Path
 		setupUI.SetupLabelUI(filePathLabel, "Arial", 36, WINDOW_WIDTH-10, 
 				Pos.BASELINE_LEFT, 10, 50, Color.BLACK);
 		
@@ -77,7 +81,7 @@ public class RestoreArticlesGUI
 		 * TextField Creations 
 		 */
 		
-		//
+		//Text Field storing file path
 		setupUI.SetupTextFieldUI(filePathText, "Arial", 18, 475, 40,
 				Pos.BASELINE_LEFT, 10, 110, true);
 		
@@ -85,15 +89,15 @@ public class RestoreArticlesGUI
 		 * Button Creations
 		 */
 		
-		//Button that ...
+		//Button that returns user back to previous page
 		setupUI.SetupButtonUI(backButton, "Arial", 11, 50, 20,
         		Pos.CENTER, 10, 10, false, Color.BLACK);
 		
-		//Button that ...
+		//Button that restores articles with overriding versus merging
 		setupUI.SetupButtonUI(overrideButton, "Arial", 24, 400, 100,
         		Pos.CENTER, 50, 175, false, Color.BLACK);
 		
-		//Button that ...
+		//Button that restores articles with merging versus overriding
 		setupUI.SetupButtonUI(mergeButton, "Arial", 24, 400, 100,
         		Pos.CENTER, 50, 300, false, Color.BLACK);
 		
@@ -113,17 +117,14 @@ public class RestoreArticlesGUI
 		{
 			public void handle(ActionEvent event) 
 			{						
-				/*
-				 * 
-				 */
-				
+				//returns user back to previous page
 				theRoot.getChildren().clear();  // Clear the current root
-				
-				Pane newRoot = new Pane(); // 
-				//ManageArticlesGUI manageArticles = new ManageArticlesGUI(theRoot); //
-				Scene newScene = new Scene(newRoot, WINDOW_WIDTH, WINDOW_HEIGHT); //
-			    Stage currentStage = (Stage) theRoot.getScene().getWindow(); // 
-			    currentStage.setScene(newScene); // 
+				//create new pane for next interface
+				Pane newRoot = new Pane();
+				//ManageArticlesGUI manageArticles = new ManageArticlesGUI(theRoot); // Returns user to previous interface
+				Scene newScene = new Scene(newRoot, WINDOW_WIDTH, WINDOW_HEIGHT); // creates new scene
+			    Stage currentStage = (Stage) theRoot.getScene().getWindow();
+			    currentStage.setScene(newScene); // sets new scene
 			}
 		});
 		
@@ -135,17 +136,35 @@ public class RestoreArticlesGUI
 		{
 			public void handle(ActionEvent event) 
 			{	
-				/*
-				 * 
-				 */
+				//retrieves file path from user
+				filePathInput = filePathText.getText();
 				
-				theRoot.getChildren().clear();  // Clear the current root
-				
-				Pane newRoot = new Pane(); // 
-				//ManageArticlesGUI manageArticles = new ManageArticlesGUI(theRoot); //
-				Scene newScene = new Scene(newRoot, WINDOW_WIDTH, WINDOW_HEIGHT); //
-			    Stage currentStage = (Stage) theRoot.getScene().getWindow(); // 
-			    currentStage.setScene(newScene); // 
+				//try and catch block needed for exception handling
+				try {
+					//Restore Articles
+					if(ArticleDatabase.restoreByOverriding(filePathInput))
+					{
+						//returns user to previous page
+						theRoot.getChildren().clear();  // Clear the current root
+						//create new pane for next interface
+						Pane newRoot = new Pane(); // 
+						//ManageArticlesGUI manageArticles = new ManageArticlesGUI(theRoot); // Returns user to previous interface
+						Scene newScene = new Scene(newRoot, WINDOW_WIDTH, WINDOW_HEIGHT); // creates new scene
+					    Stage currentStage = (Stage) theRoot.getScene().getWindow();
+					    currentStage.setScene(newScene); // sets new scene
+					}
+					else // Restore Failed
+					{
+						//print to console letting user know restoration failed
+						System.out.println("Restore by Overriding Failed!");
+					}
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
 		});
 		
@@ -156,18 +175,36 @@ public class RestoreArticlesGUI
 		mergeButton.setOnAction(new EventHandler<>()
 		{
 			public void handle(ActionEvent event) 
-			{	
-				/*
-				 * 
-				 */
+			{
+				//retrieves file path from user
+				filePathInput = filePathText.getText();
 				
-				theRoot.getChildren().clear();  // Clear the current root
-				
-				Pane newRoot = new Pane(); // 
-				//ManageArticlesGUI manageArticles = new ManageArticlesGUI(theRoot); //
-				Scene newScene = new Scene(newRoot, WINDOW_WIDTH, WINDOW_HEIGHT); //
-			    Stage currentStage = (Stage) theRoot.getScene().getWindow(); // 
-			    currentStage.setScene(newScene); // 
+				//try and catch block needed for exception handling
+				try {
+					//Restore Articles
+					if(ArticleDatabase.restoreByMerging(filePathInput))
+					{
+						//returns user to previous page
+						theRoot.getChildren().clear();  // Clear the current root
+						//create new pane for next interface
+						Pane newRoot = new Pane(); // 
+						//ManageArticlesGUI manageArticles = new ManageArticlesGUI(theRoot); // Returns user to previous interface
+						Scene newScene = new Scene(newRoot, WINDOW_WIDTH, WINDOW_HEIGHT); // Creates new scene
+					    Stage currentStage = (Stage) theRoot.getScene().getWindow();
+					    currentStage.setScene(newScene); // Sets new scene
+					}
+					else // Restore Failed
+					{
+						//print to console letting user know merging failed
+						System.out.println("Restore by Merging Failed!");
+					}
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
 		});
 	}
