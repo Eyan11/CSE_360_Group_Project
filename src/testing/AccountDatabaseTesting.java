4e4025adadbc0bc12ea8c0c9bbadd19f7a62f7cc
cadond <cadond88@gmail.com>
2024-11-26 20:20:13 -0700

    Update AccountDatabaseTesting.java

--- src/testing/AccountDatabaseTesting.java
+++ src/testing/AccountDatabaseTesting.java
@@ -1,6 +1,9 @@
 package testing;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import database.AccountDatabase;	// To test AccountDatabase methods
 import database.DatabaseManager;	// TO start and close database connection
@@ -12,67 +15,91 @@ class AccountDatabaseTesting {
 	Test AccountDatabase Getter Methods
 	
 	**********************************************************************************************/
+	/**********
+	 * Opens connection to database before all testing starts and initializes non-group database
+	 */
+	@BeforeAll
+	private static void initialization() {
+		DatabaseManager.connectToDatabase();	// Start database
+	}
+	
+	/**********
+	 * Closes connection to database when all testing is finished
+	 */
+	@AfterAll
+	public static void cleanup() {
+		DatabaseManager.closeConnection();
+	}
 	
 	@Test
 	private void testIsTableEmpty() {
-		DatabaseManager.connectToDatabase();
-		AccountDatabase.createTable();
 		AccountDatabase.deleteAllAccounts();
 		fail("Not yet implemented");
 	}
 	
 	@Test
 	private void testDoesLoginExist() {
+		AccountDatabase.deleteAllAccounts();
 		fail("Not yet implemented");
 	}
 	
 	@Test
 	private void testDoesKeyExist() {
+		AccountDatabase.deleteAllAccounts();
 		fail("Not yet implemented");
 	}
 	
 	@Test
 	private void testDoesUsernameExist() {
+		AccountDatabase.deleteAllAccounts();
 		fail("Not yet implemented");
 	}
 	
 	@Test
 	private void testDoesEmailExist() {
+		AccountDatabase.deleteAllAccounts();
 		fail("Not yet implemented");
 	}
 	
 	@Test
 	private void testIsStudentRole() {
+		AccountDatabase.deleteAllAccounts();
 		fail("Not yet implemented");
 	}
 	
 	@Test
 	private void testIsInstructorRole() {
+		AccountDatabase.deleteAllAccounts();
 		fail("Not yet implemented");
 	}
 	
 	@Test
 	private void testIsAdminRole() {
+		AccountDatabase.deleteAllAccounts();
 		fail("Not yet implemented");
 	}
 	
 	@Test
 	private void testIsAccountUpdated() {
+		AccountDatabase.deleteAllAccounts();
 		fail("Not yet implemented");
 	}
 	
 	@Test
 	private void testIsKeyExpired() {
+		AccountDatabase.deleteAllAccounts();
 		fail("Not yet implemented");
 	}
 	
 	@Test
 	private void testGetExpiration() {
+		AccountDatabase.deleteAllAccounts();
 		fail("Not yet implemented");
 	}
 	
 	@Test
 	private void testGetAllAccounts() {
+		AccountDatabase.deleteAllAccounts();
 		fail("Not yet implemented");
 	}
 	
@@ -84,41 +111,49 @@ class AccountDatabaseTesting {
 	
 	@Test
 	private void testCreateFirstAccount() {
+		AccountDatabase.deleteAllAccounts();
 		fail("Not yet implemented");
 	}
 	
 	@Test
 	private void testCreateAccountWithKey() {
+		AccountDatabase.deleteAllAccounts();
 		fail("Not yet implemented");
 	}
 	
 	@Test
 	private void testUpdateAccountInformation() {
+		AccountDatabase.deleteAllAccounts();
 		fail("Not yet implemented");
 	}
 	
 	@Test
 	private void testUpdateUserRoles() {
+		AccountDatabase.deleteAllAccounts();
 		fail("Not yet implemented");
 	}
 	
 	@Test
 	private void testInviteUser() {
+		AccountDatabase.deleteAllAccounts();
 		fail("Not yet implemented");
 	}
 	
 	@Test
 	private void testResetUser() {
+		AccountDatabase.deleteAllAccounts();
 		fail("Not yet implemented");
 	}
 	
 	@Test
 	private void testResetPassword() {
+		AccountDatabase.deleteAllAccounts();
 		fail("Not yet implemented");
 	}
 	
 	@Test
 	private void testDeleteUser() {
+		AccountDatabase.deleteAllAccounts();
 		fail("Not yet implemented");
 	}
 	
